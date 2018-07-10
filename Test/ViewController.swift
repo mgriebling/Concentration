29fe59fc4e48f2b95da30af4f567803cd5a20ca9
Michael Griebling <michael.griebling@solinst.com>
2018-07-10 09:09:37 -0400

    Initial commit.

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var flipLabel: UILabel!
+    
+    let emojis = ["🥨", "🌮", "🍕", "🍔"]
+    
+    var flipCount = 0 {
+        didSet {
+            flipLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet var cards: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cards.index(of: sender) {
+            flipCard(withEmoji: emojis[cardNumber], on: sender)
+        } else {
+            print("Unknown card encountered!")
+        }
     }
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.2931769192, green: 0.2937060595, blue: 1, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 
 }
 
